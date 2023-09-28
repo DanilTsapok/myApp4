98b23ff6b5537f5ece928be716a18c2875bfcbae
DanilTsapok <116272494+DanilTsapok@users.noreply.github.com>
2023-09-28 14:07:35 +0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,180 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import {
+  Button,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableHighlight,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 export default function App() {
+  const num = "Button";
+  const [textContent, setText] = useState("");
+  const changeText = (text) => {
+    setText(text);
+  };
+  const buttons = [
+    { text: "Hello" },
+    { text: "Hello" },
+    { text: "Hello" },
+    { text: "Hello" },
+    { text: "Hello" },
+    { text: "Hello" },
+    { text: "Hello" },
+    { text: "Hello" },
+    { text: "Hello" },
+    { text: "Hello" },
+    { text: "Hello" },
+    { text: "Hello" },
+    { text: "Hello" },
+    { text: "Hello" },
+    { text: "Hello" },
+    { text: "Hello" },
+    { text: "Hello" },
+    { text: "Hello" },
+    { text: "Hello" },
+    { text: "Hello" },
+    { text: "Hello" },
+    { text: "Hello" },
+    { text: "Hello" },
+  ];
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <SafeAreaView>
+        <ScrollView>
+          <View>
+            {buttons.map((value, index) => (
+              <Button key={index} title={value.text} />
+            ))}
+          </View>
+          <View>
+            <TextInput
+              style={styles.input}
+              placeholder="Email"
+              onChangeText={changeText}
+            />
+            <Text
+              style={{
+                flex: 1,
+                textAlign: "center",
+                color: "red",
+                fontSize: 24,
+                marginBottom: 20,
+              }}
+            >
+              {textContent}
+            </Text>
+          </View>
+        </ScrollView>
+      </SafeAreaView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  btn: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  input: {
+    borderWidth: 1,
+    marginTop: 20,
+    marginBottom: 10,
+    marginLeft: 30,
+    marginRight: 30,
+    borderColor: "grey",
+    borderRadius: 10,
+    paddingLeft: 10,
+  },
+  btnSubmit: {
+    padding: 10,
+    margin: 15,
+    backgroundColor: "green",
+    height: 40,
+    alignItems: "center",
+    borderRadius: 10,
+  },
+  btnSubmitText: {
+    color: "white",
   },
 });
+// import React, { Component } from "react";
+// import {
+//   View,
+//   Text,
+//   TouchableOpacity,
+//   TextInput,
+//   StyleSheet,
+// } from "react-native";
+
+// class App extends Component {
+//   state = {
+//     email: "",
+//     password: "",
+//   };
+//   handleEmail = (text) => {
+//     this.setState({ email: text });
+//   };
+//   handlePassword = (text) => {
+//     this.setState({ password: text });
+//   };
+//   login = (email, pass) => {
+//     alert("email: " + email + " password: " + pass);
+//   };
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <TextInput
+//           style={styles.input}
+//           underlineColorAndroid="transparent"
+//           placeholder="Email"
+//           placeholderTextColor="#9a73ef"
+//           autoCapitalize="none"
+//           onChangeText={this.handleEmail}
+//         />
+
+//         <TextInput
+//           style={styles.input}
+//           underlineColorAndroid="transparent"
+//           placeholder="Password"
+//           placeholderTextColor="#9a73ef"
+//           autoCapitalize="none"
+//           onChangeText={this.handlePassword}
+//         />
+
+//         <TouchableOpacity
+//           style={styles.submitButton}
+//           onPress={() => this.login(this.state.email, this.state.password)}
+//         >
+//           <Text style={styles.submitButtonText}> Submit </Text>
+//         </TouchableOpacity>
+//       </View>
+//     );
+//   }
+// }
+// export default App;
+
+// const styles = StyleSheet.create({
+//   container: {
+//     paddingTop: 23,
+//   },
+//   input: {
+//     margin: 15,
+//     height: 40,
+//     borderColor: "#7a42f4",
+//     borderWidth: 1,
+//   },
+//   submitButton: {
+//     backgroundColor: "#7a42f4",
+//     padding: 10,
+//     margin: 15,
+//     height: 40,
+//   },
+//   submitButtonText: {
+//     color: "white",
+//   },
+// });
